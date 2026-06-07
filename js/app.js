0adc4228eb870af16dfdb8577d6002ec05004ec5
Djayjux <153505287+Djayjux@users.noreply.github.com>
2026-06-07 17:08:54 +0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -194,7 +194,6 @@ function updateProUI() {
     if (typeof Academy !== 'undefined' && Academy.populateAcademy) Academy.populateAcademy();
     if (typeof Beyond !== 'undefined' && Beyond.populateBeyond) Beyond.populateBeyond();
 }
-    
         // Pro badge in studio topbar
         var topbarLogo = document.querySelector('.topbar-logo');
         if (topbarLogo) {
